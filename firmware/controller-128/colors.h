0c75d4725197d00e4e57755f313eb22c8d709398
Michael L Heuer <heuermh@acm.org>
2021-01-15 17:20:50 -0600

    add firmware directory

--- firmware/controller-128/colors.h
+++ firmware/controller-128/colors.h
@@ -19,3 +19,40 @@
     along with controller-128.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+#ifndef colors_h
+#define colors_h
+
+#include "Arduino.h"
+
+class rgb {
+  public:
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  rgb();
+  rgb(uint8_t r, uint8_t g, uint8_t b);
+  //hsv to_hsv();
+  //hsv to_hsv(hsv out);
+};
+
+class hsv {
+  public:
+  double h;
+  double s;
+  double v;
+  hsv();
+  hsv(double h, double s, double v);
+  //rgb to_rgb();
+  //rgb to_rgb(rgb out);
+};
+
+rgb to_rgb(hsv in);
+hsv to_hsv(rgb in);
+rgb to_rgb(hsv in, rgb out);
+hsv to_hsv(rgb in, hsv out);
+
+rgb map_qual(int x, rgb colors[], int len);
+rgb map_seq(int x, int min, int max, rgb colors[], int len);
+rgb map_div(int x, int min, int max, rgb colors[], int len);
+
+#endif
--- firmware/controller-128/controller-128.ino
+++ firmware/controller-128/controller-128.ino
@@ -19,3 +19,38 @@
     along with controller-128.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+#include "colors.h"
+
+void setup() {
+  rgb c1 = rgb(10, 20, 30);
+  hsv c2 = hsv(0.6d, 0.4d, 0.2d);
+
+  hsv c3 = to_hsv(c1);
+  rgb c4 = to_rgb(c2);
+
+  hsv c5;
+  c5 = to_hsv(c1, c5);
+  rgb c6;
+  c6 = to_rgb(c2, c6);
+
+  int len = 8;
+  rgb colors[] = {
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30),
+    rgb(10, 20, 30)
+  };
+
+  rgb c7 = map_seq(2, 0, 127, colors, len);
+  rgb c8 = map_div(2, -127, 127, colors, len);
+  rgb c9 = map_div(-2, -127, 127, colors, len);
+  rgb c0 = map_qual(2, colors, len);
+}
+
+void loop() {
+  // empty
+}
